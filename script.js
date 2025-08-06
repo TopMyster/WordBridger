a9210b8716bd0bb1d6aebed0fdc8f7126dce5ac2
TopMyster <topmyster@outlook.com>
2025-08-06 08:11:47 -0500

    adds speach

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let API_KEY
 
 window.onload = function() {
-    if(!API_KEY) {
-        API_KEY = window.prompt('Enter your OpenAI API key.')
-        localStorage.setItem("API_KEY", API_KEY)
+    if(!localStorage.getItem("API_KEY")) {
+        document.getElementById('getkey').style.display = 'block'
+        document.getElementById('submition').addEventListener('click', function() {
+            API_KEY = document.getElementById('gotkey').value
+            localStorage.setItem("API_KEY", API_KEY)
+            document.getElementById('getkey').style.display = 'none'
+        })
     }
 }
 
@@ -39,22 +43,31 @@ function speak(text, pitch = 0.3, rate = 0.3) {
     window.speechSynthesis.speak(utterance)
 }
 
-async function ask() {
-    const transcript = localStorage.getItem("transcript");
+function speech() {
+    txt = document.getElementById('result').value
+    speak(txt, 0.8, 0.8)
+}
+
+document.getElementById('translatebtn').addEventListener('click', translate())
+
+async function translate() {
+    const API_KEY = localStorage.getItem("API_KEY")
+    const language = document.getElementById('language').value
+    const transcript = document.getElementById('userinput').value
     console.log("Sending transcript:", transcript);
     try {
       const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "Bearer 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",
+          "Authorization": `Bearer ${API_KEY}`,
         },
         body: JSON.stringify({
           model: "deepseek/deepseek-r1-0528:free",
           messages: [
             {
               role: "user",
-              content: `Answer the question or statement, ${transcript}.(Speak English by default and don't include so many emojis and don't declare everything you do before doing it)`,
+              content: `In the first sentance only Translate the ${transcript} to ${language}. 2 lines under the 1st sentance write a short 2 sentance definitions of the translation.`,
             },
           ],
         }),
@@ -64,10 +77,7 @@ async function ask() {
       console.log("Response data:", data);
       
       if (data.choices && data.choices.length > 0) {
-        document.getElementById('content').style.animation = 'aiactive 1.3s infinite;'
-        txt =  data.choices[0].message.content || data.choices[0].text.content
-        speak(txt, 0.8, 0.8)
-        console.log("ChatGPT reply:", data.choices[0].message.content || data.choices[0].text.content);
+        document.getElementById('result').value = data.choices[0].message.content || data.choices[0].text.content
       } else {
         console.error("Unexpected response format:", data);
       }

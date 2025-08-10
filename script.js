4c566386dcd5a088ad62f250e805ae30d4f66ebd
TopMyster <topmyster@outlook.com>
2025-08-10 12:14:13 -0500

    adds groq link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     if (!localStorage.getItem("API_KEY")) {
-        const apiKey = window.prompt('Enter your API_KEY here. Get the API_KEY from https://huggingface.co/')
+        const apiKey = window.prompt('Enter your API_KEY here. Get the API_KEY from https://console.groq.com/keys')
         if (apiKey) {
             localStorage.setItem("API_KEY", apiKey)
         } else {
